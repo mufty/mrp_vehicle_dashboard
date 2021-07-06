97e06d6027691c32f258b7e0b86720174c14a563
mufty <gubriansky@sernato.sk>
2021-07-06 14:27:50 +0200

    include all images

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,14 +12,7 @@ dependencies {
 }
 
 files {
-    'ui/icons/030-gas-fuel_red.png',
-    'ui/icons/030-gas-fuel_white.png',
-    'ui/icons/033-seat-belt_red.png',
-    'ui/icons/035-car-oil_red.png',
-    'ui/icons/035-car-oil_white.png',
-    'ui/icons/007-motor_orange.png',
-    'ui/icons/007-motor_red.png',
-    'ui/icons/007-motor_white.png',
+    'ui/icons/*.png',
     'ui/font/made-black.otf',
     'ui/scripts/hud.js',
     'ui/lib/gauge.min.js',
